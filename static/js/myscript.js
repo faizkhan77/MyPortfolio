414a23a4d0471b4abf85348d91812b80b99a30f6
faiz <randygaming.net@gmail.com>
2023-11-23 06:28:56 +0530

    at 1:47:44

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -26,13 +26,13 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-            // // active sections for animation on scroll
-            // sec.classList.add('show-animate');
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
         }
-        // // if u want to use animation that repeas on scroll use this
-        // else{
-        //     sec.classList.remove('show-animate');
-        // }
+        // if u want to use animation that repeas on scroll use this
+        else{
+            sec.classList.remove('show-animate');
+        }
     })
 
     // sticky header
